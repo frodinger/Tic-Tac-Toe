b1374d230356aae339048e2c84414eef564ced68
frodinger <frej.hedlin@gmail.com>
2024-04-18 14:01:22 +0200

    you are at move

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -70,14 +70,19 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+    let yourMove;
     if (move > 0) {
       description = 'Go to move #' + move;
+      if (move === currentMove) {
+        yourMove = "You are at move #" + move;
+      }
     } else {
       description = 'Go to game start';
     }
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
+        <p>{yourMove}</p>
       </li>
     );
   });
