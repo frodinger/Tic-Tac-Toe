eb86cc1fab79f043bc4becdd6cd301d509a79d93
frodinger <frej.hedlin@gmail.com>
2024-04-23 13:43:05 +0200

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,19 @@
 import LandingPage from "./pages/LandingPage";
+import About from "./pages/About";
 import Header from "./Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <>
-  <Header />
-  <LandingPage />
-  </>
-}
+  return (
+  <BrowserRouter>
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+  );
+};
 
 export default App;
